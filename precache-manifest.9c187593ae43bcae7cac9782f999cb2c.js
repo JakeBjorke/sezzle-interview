de0c9777bcb38f17554ebe87c23a36751f4ed5a7
Jake Bjorke <jrbjorke@gmail.com>
2020-02-25 23:14:38 -0600

    Updates

---
+++ precache-manifest.9c187593ae43bcae7cac9782f999cb2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21b4339131fe047c4e6e8654fb3a965",
+    "url": "/sezzle-interview/index.html"
+  },
+  {
+    "revision": "5d62b7c07e6c063a6f9a",
+    "url": "/sezzle-interview/static/css/main.cdca6ce7.chunk.css"
+  },
+  {
+    "revision": "65d37063c8d83c7ccfe1",
+    "url": "/sezzle-interview/static/js/2.3b34ba16.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sezzle-interview/static/js/2.3b34ba16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d62b7c07e6c063a6f9a",
+    "url": "/sezzle-interview/static/js/main.7f479630.chunk.js"
+  },
+  {
+    "revision": "94f6bac19d0ade452fce",
+    "url": "/sezzle-interview/static/js/runtime-main.0dca58e2.js"
+  }
+]);
